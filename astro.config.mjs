203e8bf65d9772fdfdc07d2f351e17d89826f7e9
unknown <zhuizheye@hotmail.com>
2026-01-18 22:01:44 +0800

    更新项目文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://streetdogbmx.wiki',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
   output: 'static',
   build: {
     inlineStylesheets: 'always',
